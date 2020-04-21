6fa2f86b939f3c5abc06c5c2ab75f2bedd3608e1
Богдан <zvyagintsevbi@pik.ru>
2020-04-21 06:03:43 +0300

    fix: change crt path

--- ormconfig.js
+++ ormconfig.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 
 const username = process.env.POSTGRES_USER || 'user1';
 const password = process.env.POSTGRES_PASSWORD || 'testpass';
-const crt = fs.readFileSync('./root.crt') || undefined;
+const crt = fs.readFileSync('root.crt') || undefined;
 
 module.exports = {
   type: 'mysql',
   host: 'rc1a-ulj3bqjzol288q5c.mdb.yandexcloud.net',
   port: 3306,
-  username: 'user1',
-  password: 'testpass',
+  username,
+  password,
   database: 'db1',
   ssl: crt
     ? {
